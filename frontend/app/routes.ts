243235c37c388e678b935ec9f2062e05ca3745ec
Adithep <adithep.su@ku.th>
2025-09-08 21:53:48 +0700

    try make home screen

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -1,5 +1,6 @@
 import { type RouteConfig, index , route } from "@react-router/dev/routes";
 
 export default [index("routes/home.tsx"),
-        route("/login", "routes/login.tsx")
+        route("/login", "routes/login.tsx"),
+        route("/home_screen", "routes/home_screen.tsx")
 ] satisfies RouteConfig;
