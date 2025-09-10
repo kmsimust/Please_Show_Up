0178645ed21ae617adcc3518abcf9adde787a6c0
Adithep <adithep.su@ku.th>
2025-09-10 16:10:27 +0700

    add login and sign up

---
+++ frontend/app/routes/signup.tsx
@@ -0,0 +1,13 @@
+import type { Route } from "./+types/home";
+import { Signup } from '../signup/signup';
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Home() {
+  return <Signup />;
+}
