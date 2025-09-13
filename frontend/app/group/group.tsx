749b5f2030731ff27f2b97068ed419d8af5f7e46
JirakornChaitanaporn <jirakorn.c@ku.th>
2025-09-13 17:06:32 +0700

    rough draft of group page

--- frontend/app/group/group.tsx
+++ frontend/app/group/group.tsx
@@ -1,11 +1,45 @@
+import logo from './logo.png';
+
 export function Group() {
+  const handleCreateGroup = () => {
+  
+  };
+
   return (
         <div>
-            <div>{/*header*/}
-                <img src="" alt="" />
-            </div>
-            <div>{/*create group*/}
+            <div
+              style={{
+                backgroundColor: 'rgb(0, 191, 99)',
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                padding: '10px 5%'
+              }}
+            >
+              <img
+                style={{height: '125px'}}
+                src={logo} alt="Logo" 
+              />
 
+              <div style={{ display: 'flex', gap: '20px' }}>
+                <button>Login</button>
+                <button>Sign Up</button>
+              </div>
+            </div>
+
+            <div
+                style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                }}
+            >
+              <button 
+                style={{backgroundColor: 'rgb(0, 191, 99)',
+                        margin: '69px',
+                        padding: '7px 16px'
+                }}
+                onClick = {handleCreateGroup}>Create a group</button>
             </div>
             <div>{/*groups*/}
 
