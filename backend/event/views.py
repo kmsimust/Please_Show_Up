39cfdf1ca872185b99003d4d490aea47038e6bd5
JirakornChaitanaporn <jirakorn.c@ku.th>
2025-11-11 16:14:34 +0700

    feat(event):Add time to control user's action

--- backend/event/views.py
+++ backend/event/views.py
@@ -10,6 +10,7 @@ from .models import Event
 from group_member.models import GroupMember
 from group_member.serializers import GroupMemberSerializer
 from .serializers import EventSerializer, EventSerializerSave
+from util.create_date import create_date
 
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
@@ -37,6 +38,8 @@ def get_event_by_group_id(request, group_id):
 def create_event(request):
     body = request.data
 
+    if (datetime.strptime(body["start_date"], '%Y-%m-%d').date() < datetime.now().date()):
+        return Response({"message":"Event can only start today and the day after"},status = status.HTTP_400_BAD_REQUEST)
 
     body["end_date"] = str(datetime.strptime(body["start_date"], '%Y-%m-%d').date() + timedelta(days=6)) #"2025-11-26"
 
@@ -44,25 +47,7 @@ def create_event(request):
     if serializer.is_valid():
         serializer.save() # INSERT 
 
-        serializer_dict = dict(serializer.data)
-        group_id = serializer_dict["group"]
-        group_member = GroupMember.objects.filter(group = group_id)
-        group_member_serializer = GroupMemberSerializer(group_member, many = True)
-        #print(group_member_serializer)
-        group_member_serializer_list = list(group_member_serializer.data)
-        # {} # dict
-        start_date_str = serializer_dict["start_date"] #"2025-11-10"
-        start_date = datetime.strptime(start_date_str, '%Y-%m-%d').date() # type date
-
-        for group_member_serializer_dict in group_member_serializer_list:
-            for i in range(7):
-                current_date = start_date + timedelta(days=i)
-                available_date_serializer = AvailableDateSerializerSave(data = {"event":serializer_dict["id"],\
-                                                                             "group_member": group_member_serializer_dict["id"],\
-                                                                            "date":f"{str(current_date)}",
-                                                                              "status":"maybe"})
-                if available_date_serializer.is_valid(): 
-                    available_date_serializer.save()
+        create_date(serializer)
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -76,6 +61,15 @@ def update_event(request, pk):
     except Event.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
     
+    date_str =  dict(EventSerializer(event).data)["end_date"]
+    if datetime.now().date() > datetime.strptime(date_str, '%Y-%m-%d').date():
+        return Response({"message":"Cannot edit anything after the end_date"} ,status = status.HTTP_400_BAD_REQUEST)
+    
+    if (datetime.strptime(body["start_date"], '%Y-%m-%d').date() < datetime.now().date()):
+        return Response({"message":"Event can only start today and the day after"},status = status.HTTP_400_BAD_REQUEST)
+    
+
+    body["end_date"] = str(datetime.strptime(body["start_date"], '%Y-%m-%d').date() + timedelta(days=6))
     serializer = EventSerializerSave(event, data=body)
     if serializer.is_valid():
         serializer.save() # UPDATE
@@ -89,6 +83,10 @@ def update_name_and_description(request, pk):
         event = Event.objects.get(pk=pk)
     except Event.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    date_str =  dict(EventSerializer(event).data)["end_date"]
+    if datetime.now().date() > datetime.strptime(date_str, '%Y-%m-%d').date():
+        return Response({"message":"Cannot edit anything after the end_date"} ,status = status.HTTP_400_BAD_REQUEST)
 
     body = request.data
     serializer = EventSerializerSave(event, data={"name": body["name"], "description": body["description"]}, partial=True)
@@ -106,8 +104,18 @@ def update_start_and_end_date(request, pk):
     except Event.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
     
+
+    date_str =  dict(EventSerializer(event).data)["end_date"]
+    if datetime.now().date() > datetime.strptime(date_str, '%Y-%m-%d').date():
+        return Response({"message":"Cannot edit anything after the end_date"} ,status = status.HTTP_400_BAD_REQUEST)
+
     body = request.data
-    serializer = EventSerializerSave(event, data = {"start_date": body["start_date"], "end_date": body["end_date"]}, partial = True)
+
+    if (datetime.strptime(body["start_date"], '%Y-%m-%d').date() < datetime.now().date()):
+        return Response({"message":"Event can only start today and the day after"},status = status.HTTP_400_BAD_REQUEST)
+    
+    end_date = str(datetime.strptime(body["start_date"], '%Y-%m-%d').date() + timedelta(days=6))
+    serializer = EventSerializerSave(event, data = {"start_date": body["start_date"], "end_date": end_date, "event_date":None}, partial = True)
 
     if serializer.is_valid():
         serializer.save()
@@ -116,8 +124,10 @@ def update_start_and_end_date(request, pk):
             available_date.delete()
         except AvailableDate.DoesNotExist:
             print(f"available date not found at event_id:{pk}")
-        
-        
+
+
+        create_date(serializer)
+
         return Response(serializer.data)
     return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
@@ -128,12 +138,18 @@ def update_event_date(request, pk):
         event = Event.objects.get(pk=pk)
     except Event.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
+
+    date_str =  dict(EventSerializer(event).data)["end_date"]
+    if datetime.now().date() > datetime.strptime(date_str, '%Y-%m-%d').date():
+        return Response({"message":"Cannot edit anything after the end_date"} ,status = status.HTTP_400_BAD_REQUEST)
     
     body = request.data
-    event_serializer = EventSerializer(event, many = True)
-    start_date = datetime.strptime(event_serializer["start_date"], '%Y-%m-%d').date()
-    end_date = datetime.strptime(event_serializer["end_date"], '%Y-%m-%d').date()
-    event_date = datetime.strptime(event_serializer["event_date"], '%Y-%m-%d').date()
+    event_serializer = EventSerializer(event)
+
+    # print("start_date", event_serializer["start_date"].value)
+    start_date = datetime.strptime(event_serializer["start_date"].value, '%Y-%m-%d').date()
+    end_date = datetime.strptime(event_serializer["end_date"].value, '%Y-%m-%d').date()
+    event_date = datetime.strptime(body["event_date"], '%Y-%m-%d').date()
 
     if not(start_date <= event_date <= end_date):
         return Response({"message":"event date must be between start and end date"}, status = status.HTTP_400_BAD_REQUEST)
---
+++ backend/util/create_date.py
@@ -0,0 +1,24 @@
+from group_member.models import GroupMember
+from group_member.serializers import GroupMemberSerializer
+from datetime import datetime, timedelta
+from available_date.serializer import AvailableDateSerializerSave
+
+def create_date(serializer):
+    serializer_dict = dict(serializer.data)
+    group_member = GroupMember.objects.filter(group = serializer_dict["group"])
+    
+    if len(group_member) > 0:
+        group_member_serializer = GroupMemberSerializer(group_member, many = True)
+        group_member_serializer_list = list(group_member_serializer.data)
+
+        start_date = datetime.strptime(serializer_dict["start_date"], '%Y-%m-%d').date() # type date
+
+        for group_member_serializer_dict in group_member_serializer_list: #add 7 days in available date table
+            for i in range(7):
+                current_date = start_date + timedelta(days=i)
+                available_date_serializer = AvailableDateSerializerSave(data = {"event": serializer_dict["id"],\
+                                                                                "group_member": group_member_serializer_dict["id"],\
+                                                                                "date": str(current_date),
+                                                                                "status":"maybe"})
+                if available_date_serializer.is_valid(): 
+                    available_date_serializer.save()
